73d35cf4c7148e981e5fe52dc1d6c997e2a71d18
Abhijit Patil <157373320+patilabhi20@users.noreply.github.com>
2024-06-21 14:34:36 +0530

    Create Forward_kinematics.py

---
+++ Kinematics/Forward_kinematics.py
@@ -0,0 +1,57 @@
+import os
+import sys
+sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'joint_control'))
+
+from numpy.matlib import matrix, identity
+
+from recognize_posture import PostureRecognitionAgent
+
+
+class ForwardKinematicsAgent(PostureRecognitionAgent):
+    def __init__(self, si_ip='localhost',
+                 si_port=3100,
+                 teamname='ABC',
+                 player_id=0,
+                 sync_mode=True):
+        super(ForwardKinematicsAgent, self).__init__(simspark_ip, simspark_port, teamname, player_id, sync_mode)
+        self.transforms = {n: identity(4) for n in self.joint_names}
+
+        # chains defines the name of chain and joints of the chain
+        self.chains = {'Head': ['HeadYaw', 'HeadPitch']
+                       # YOUR CODE HERE
+                       }
+
+    def think(self, perception):
+        self.forward_kinematics(perception.joint)
+        return super(ForwardKinematicsAgent, self).think(perception)
+
+    def local_trans(self, joint_name, joint_angle):
+        '''calculate local transformation of one joint
+
+        :param str joint_name: the name of joint
+        :param float joint_angle: the angle of joint in radians
+        :return: transformation
+        :rtype: 4x4 matrix
+        '''
+        T = identity(4)
+        # YOUR CODE HERE
+
+        return T
+
+    def forward_kinematics(self, joints):
+        '''forward kinematics
+
+        :param joints: {joint_name: joint_angle}
+        '''
+        for chain_joints in self.chains.values():
+            T = identity(4)
+            for joint in chain_joints:
+                angle = joints[joint]
+                Tl = self.local_trans(joint, angle)
+                # YOUR CODE HERE
+
+                self.transforms[joint] = T
+
+if __name__ == '__main__':
+    agent = ForwardKinematicsAgent()
+    agent.run()
